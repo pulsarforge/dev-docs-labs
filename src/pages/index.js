dcf7a7431f783e88a67b31b7b912d612a3a1308a
pulsarforge <omgworkshopfire@gmail.com>
2023-11-16 18:50:08 +0100

    update link

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="https://main.pulsarforge.io/docs/shipping">
+            to="https://main.pulsarforge.io/docs/Mind">
               <img src='/img/devs-docs.png' alt='developer docs' style={{width: 700}}/>
           </Link>
         </div>
